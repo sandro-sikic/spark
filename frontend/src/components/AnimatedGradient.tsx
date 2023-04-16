8890ce73ce23601b48106cdbaa176cce74f8b595
Sandro Sikic <sandro.sikic@gmail.com>
2023-04-17 00:12:44 +0200

    feat: added animation to animated components

--- frontend/src/components/AnimatedGradient.tsx
+++ frontend/src/components/AnimatedGradient.tsx
@@ -13,20 +13,73 @@ export default function AnimatedGradient({
 	top?: number | null;
 	left?: number | null;
 }) {
-	return (
-		<div
-			style={{
-				position: 'absolute',
-				background: `radial-gradient(50% 50% at 50% 50%, ${fromColor} 0%, rgba(0, 0, 0, 0) 100%)`,
-				zIndex: '0',
-				height: '230vh',
-				width: '260vh',
-				right: right + 'rem',
-				left: left + 'px',
-				top: top + 'px',
-				bottom: bottom + 'rem',
+	const animationDurationSeconds = 10;
 
-			}}
-		/>
+	return (
+		<>
+			<style jsx>{`
+				@keyframes animatedGradientAnimationFrom {
+					0% {
+						opacity: 0;
+					}
+					50% {
+						opacity: 1;
+					}
+					100% {
+						opacity: 0;
+					}
+				}
+
+				@keyframes animatedGradientAnimationTo {
+					0% {
+						opacity: 1;
+					}
+					50% {
+						opacity: 0;
+					}
+					100% {
+						opacity: 1;
+					}
+				}
+			`}</style>
+
+			<div
+				style={{
+					position: 'absolute',
+					zIndex: '0',
+					height: '230vh',
+					width: '260vh',
+					background: `radial-gradient(
+							50% 50% at 50% 50%,
+							${fromColor} 0%,
+							rgba(0, 0, 0, 0) 100%
+						)`,
+					right: right + 'rem',
+					left: left + 'rem',
+					top: top + 'rem',
+					bottom: bottom + 'rem',
+					animation: `animatedGradientAnimationFrom ${animationDurationSeconds}s ease-in-out infinite`,
+				}}
+			/>
+
+			<div
+				style={{
+					position: 'absolute',
+					zIndex: '0',
+					height: '230vh',
+					width: '260vh',
+					background: `radial-gradient(
+							50% 50% at 50% 50%,
+							${toColor} 0%,
+							rgba(0, 0, 0, 0) 100%
+						)`,
+					right: right + 'rem',
+					left: left + 'rem',
+					top: top + 'rem',
+					bottom: bottom + 'rem',
+					animation: `animatedGradientAnimationTo ${animationDurationSeconds}s ease-in-out infinite`,
+				}}
+			/>
+		</>
 	);
 }
