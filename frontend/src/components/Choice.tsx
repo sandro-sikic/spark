dd11fe1db62b668c15e6e76e527e1add0c8d33b8
Sandro Sikic <sandro.sikic@gmail.com>
2023-04-20 18:32:52 +0200

    fix: updated styling to match figma and work on mobile

--- frontend/src/components/Choice.tsx
+++ frontend/src/components/Choice.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 interface ChoiceProps {
 	text: string;
-	selected: string;
+	selected: boolean;
 	image: string;
 	onClick: () => void;
 	type: string;
+	className?: string;
 }
 
 type StylesTypes = {
-	[key: string]: {},
-}
+	[key: string]: {};
+};
 
 export default function Choice({
 	text,
@@ -18,46 +19,61 @@ export default function Choice({
 	image,
 	onClick,
 	type,
+	className,
 }: ChoiceProps) {
 	const styles: StylesTypes = {
-		'category': {
-			minWidth: '10rem',
-			height: '10rem',
+		action: {
+			borderRadius: '40% 10% 40% 10%',
 		},
-		'action': {
-			borderRadius: '60px 10px 60px 10px',
-			minWidth: '8rem',
+		hero: {
+			borderRadius: '100%',
 		},
-		'hero': {
-			borderRadius: '150px',
-			minWidth: '8rem',
+		setting: {
+			borderRadius: '10%',
 		},
-		'setting': {
-			borderRadius: '20px',
-			minWidth: '8rem',
-		}
-	}
+	};
 
-	if (type === 'category') {
+	if (type === 'genre') {
 		return (
-			<button className='pr-40 pb-20' onClick={onClick}>
-				<svg className=' w-40' viewBox="0 0 152 152">
-					<defs >
-						<clipPath id='category'>
-							<path d="M0 76C0 14.44 14.44 0 76 0C137.56 0 152 14.44 152 76C152 137.56 137.56 152 76 152C14.44 152 0 137.56 0 76Z" fill="" />
+			<button
+				className={`flex flex-col items-center flex-shrink-0 active:scale-105 hover:scale-110  transition-transform ${className}`}
+				onClick={onClick}
+			>
+				<svg className="w-20 h-20 sm:w-40 sm:h-40" viewBox="0 0 152 152">
+					<defs>
+						<clipPath id="category">
+							<path
+								d="M0 76C0 14.44 14.44 0 76 0C137.56 0 152 14.44 152 76C152 137.56 137.56 152 76 152C14.44 152 0 137.56 0 76Z"
+								fill=""
+							/>
 						</clipPath>
 					</defs>
-					<image clip-path='url(#category)' style={styles[type]} xlinkHref={image} alt="" />
+
+					<image
+						className="w-40 h-40"
+						clip-path="url(#category)"
+						preserveAspectRatio="xMidYMid slice"
+						xlinkHref={image}
+					/>
 				</svg>
-				<span className='text-2xl'>{text}</span>
+
+				<span className="text-lg sm:text-2xl tracking-wider mt-2">{text}</span>
 			</button>
-		)
+		);
 	}
 
 	return (
-		<button onClick={onClick}>
-			<img className='w-40 h-40' src={image} style={styles[type]} alt="" />
-			<span className='text-2xl'>{text}</span>
+		<button
+			className={`flex flex-col items-center flex-shrink-0 active:scale-105 hover:scale-110  transition-transform ${className}`}
+			onClick={onClick}
+		>
+			<img
+				className="w-20 h-20 sm:w-40 sm:h-40"
+				src={image}
+				style={styles[type]}
+			/>
+
+			<span className="text-lg sm:text-2xl tracking-wider mt-2">{text}</span>
 		</button>
 	);
 }
