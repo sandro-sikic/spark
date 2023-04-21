a83b8b329fcd8cccaf98158e941285bd038d454f
Sandro Sikic <sandro.sikic@gmail.com>
2023-04-21 18:10:46 +0200

    feat: connected to books api

--- frontend/src/pages/bookshelf.tsx
+++ frontend/src/pages/bookshelf.tsx
@@ -1,8 +1,21 @@
 import Book from '@/components/Book';
 import BackButton from '@/components/BackButton';
 import AnimatedGradient from '@/components/AnimatedGradient';
+import Link from 'next/link';
 
-export default function Bookshelf() {
+export async function getServerSideProps() {
+	let books: any = await fetch(`http://127.0.0.1:8000/books`);
+
+	books = await books.json();
+
+	return {
+		props: {
+			books,
+		},
+	};
+}
+
+export default function Bookshelf({ books }: { books: any }) {
 	return (
 		<>
 			<AnimatedGradient fromColor="#234463" toColor="#522363" />
@@ -17,13 +30,19 @@ export default function Bookshelf() {
 				</header>
 
 				<div className="mx-auto max-w-screen-md w-full pt-44">
-					<div className="flex items-center">
-						<Book image="https://edit.org/images/cat/book-covers-big-2019101610.jpg" />
-						<div className="flex-col ml-6 sm:ml-12">
-							<h1 className="text-3xl">The adventures of Lily and Leo</h1>
-							<h2>Fantasy</h2>
-						</div>
-					</div>
+					{books.map((book: any) => (
+						<Link
+							className="flex items-center mb-4"
+							key={book._id}
+							href={`/book/${book._id}/0`}
+						>
+							<Book image="https://edit.org/images/cat/book-covers-big-2019101610.jpg" />
+							<div className="flex-col ml-6 sm:ml-12">
+								<h1 className="text-3xl">The adventures of Lily and Leo</h1>
+								<h2>Fantasy</h2>
+							</div>
+						</Link>
+					))}
 				</div>
 			</main>
 		</>
