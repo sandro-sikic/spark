e64f60f83b2700054a15f4c2330121dc2c6ca61d
Sandro <sandro.sikic@gmail.com>
2023-07-03 04:11:44 +0200

    feat: ignore build errors

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
 
-module.exports = nextConfig
+	ignoreBuildErrors: true,
+
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+};
+
+module.exports = nextConfig;
